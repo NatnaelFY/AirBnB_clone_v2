2ac8a215a53148deff4b4135d421a51c57b02d2e
NatnaelFY <natfiky2020@gmail.com>
2022-11-13 23:25:23 +0300

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello route!'
+<<<<<<< HEAD
+"""the `0-hello_route` module
+starts a flask web application listening on `0.0.0.0:5000`
 """
+
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_route():
-    """Displays 'Hello route!'"""
-    return "Hello route!"
+@app.route("/")
+def hello():
+    """returns `Hello HBNB!` message"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
+=======
+"""
+starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
